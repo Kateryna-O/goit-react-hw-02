13f50b5aaa37996f9beab3b621c01bd1fb5b92e6
Kateryna-O <osadcukkata88@gmail.com>
2024-05-22 23:39:53 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Description from "./components/Description/Descriotion.jsx";
+
+function App() {
+  return (
+    <>
+      <Description />
+    </>
+  );
+}
+
+export default App;
